07fd7812f9ec89b62bbc5c9482b87a93b2efdb9d
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-02 12:14:56 +0100

    Update RF_DFlipFlop.vhd

--- src/RF_DFlipFlop/RF_DFlipFlop.vhd
+++ src/RF_DFlipFlop/RF_DFlipFlop.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer:
 -- 
--- Create Date: 20.09.2023 15:22:51
+-- Create Date: 20.09.2023 14:53:59
 -- Design Name: 
--- Module Name: D_Flip_Flop_PEdge_TB - Simulation
+-- Module Name: D_Flip_Flop_PEdge - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,53 +21,33 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity D_Flip_Flop_PEdge_22336157_TB is
---  we don't need ports
-end D_Flip_Flop_PEdge_22336157_TB;
-
-architecture Simulation of D_Flip_Flop_PEdge_22336157_TB is
-   -- Component Declaration for the Unit Under Test (UUT)
-    COMPONENT D_Flip_Flop_PEdge_22336157
+entity D_Flip_Flop_PEdge_22336157 is
     Port ( CLK, D : in STD_LOGIC;
            Q, Q_not : out STD_LOGIC);
-    END COMPONENT;
-   --Inputs Signals 
-   signal CLK_TB, D_TB : STD_LOGIC := '0';
-   --Output Signal   
-   signal Q_TB, Q_not_TB : STD_LOGIC := '0';
-   -- StudentID e.g. 26 33 57 25(DEC) = 1 91 D9 ED(HEX)
-   constant StudentID : STD_LOGIC_VECTOR (27 downto 0) := x"154D29D";
-   constant PERIOD : time := 200ns;  
+end D_Flip_Flop_PEdge_22336157;
 
+architecture Behavioral of D_Flip_Flop_PEdge_22336157 is
+
+   signal Q_t, Q_not_t, X, S, R, Y, Z : std_logic;
+   
+   -- Propagation Delay
+   constant AND_gate_delay : Time := 8ns;      -- least significant digit 6 = 5 + 1
+   constant NAND_gate_delay : Time := 6ns;     -- next more significant digit 3 = 2 + 1
+   constant OR_gate_delay : Time := 2ns;       -- next more significant digit 8 = 7 + 1
+   constant NOR_gate_delay : Time := 7ns;      -- next more significant digit 6 = 5 + 1
+   constant XOR_gate_delay : Time := 4ns;      -- next more significant digit 4 = 3 + 1
+   constant XNOR_gate_delay : Time := 4ns;     -- next more significant digit 4 = 3 + 1
+   constant NOT_gate_delay : Time := 3ns;      -- next more significant digit 7 = 6 + 1
+   
 begin
-
-   -- Instantiate the Unit Under Test (UUT)
-   uut: D_Flip_Flop_PEdge_22336157 PORT MAP (
-          CLK => CLK_TB,
-          D => D_TB,
-          Q => Q_TB, 
-          Q_not => Q_not_TB
-        );
-        
-   Clk_TB <= not CLK_TB after PERIOD/2;
-   
-   stim_proc: process
-   begin
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '0' after PERIOD/4;                 -- Case A
-   
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '0' after PERIOD/4;                 -- Case B
-      
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '1' after PERIOD/4;                 -- Case C
-      
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '0' after PERIOD/4;                 -- Case D
-      
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '1' after PERIOD/4;                 -- Case E
-      
-      wait until CLK_TB'event and CLK_TB='1';
-   end process;          
-end Simulation;
+    X <= Y nand S after NAND_gate_delay;
+    S <= X nand CLK after NAND_gate_delay;
+    Z <= S and CLK after AND_gate_delay;
+    R <= Z nand Y after NAND_gate_delay;
+    Y <= R nand D after NAND_gate_delay;
+    Q_t <= S nand Q_not_t after NAND_gate_delay;
+    Q_not_t <= R nand Q_t after NAND_gate_delay;
+    Q <= Q_t;
+    Q_not <= Q_not_t;
+    
+end Behavioral;
