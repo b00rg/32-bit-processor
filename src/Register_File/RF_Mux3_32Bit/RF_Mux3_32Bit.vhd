f2a421b4ad69692bc02b8d0ab9cab2699cb34168
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-02 17:47:12 +0100

    Update RF_Mux3_32Bit.vhd

--- src/Register_File/RF_Mux3_32Bit/RF_Mux3_32Bit.vhd
+++ src/Register_File/RF_Mux3_32Bit/RF_Mux3_32Bit.vhd
@@ -40,18 +40,32 @@ architecture Structural of RF_Mux3_32Bit is
     end component;
 
 begin
-    -- Generate 32 instances of Mux_3_to_1
-    gen_mux: for i in 0 to 31 generate
-        MuxInst: Mux3_1Bit
-        port map (
-            I0 => I0(i),          -- ith bit of input I0
-            I1 => I1(i),          -- ith bit of input I1
-            I2 => I2(i),          -- ith bit of input I2
-            S0 => S0,             -- Selection signal S0
-            S1 => S1,             -- Selection signal S1
-            Y  => Y(i)            -- ith bit of output Y
-        );
-    end generate gen_mux;
+ -- Invert the selection signals
+    S0_not <= not S0 after 3 ns;
+    S1_not <= not S1 after 3 ns;
+    S2_not <= not S2 after 3 ns;
+    S3_not <= not S3 after 3 ns;
 
-end Structural;
+    -- AND gates to choose the correct input
+    and0  <= I0  and S0_not and S1_not and S2_not and S3_not after 4 ns; 
+    and1  <= I1  and S0 and S1_not and S2_not and S3_not after 4 ns;
+    and2  <= I2  and S0_not and S1 and S2_not and S3_not after 4 ns;
+    and3  <= I3  and S0 and S1 and S2_not and S3_not after 4 ns;
+    and4  <= I4  and S0_not and S1_not and S2 and S3_not after 4 ns;
+    and5  <= I5  and S0 and S1_not and S2 and S3_not after 4 ns;
+    and6  <= I6  and S0_not and S1 and S2 and S3_not after 4 ns;
+    and7  <= I7  and S0 and S1 and S2 and S3_not after 4 ns;
+    and8  <= I8  and S0_not and S1_not and S2_not and S3 after 4 ns; 
+    and9  <= I9  and S0 and S1_not and S2_not and S3 after 4 ns;
+    and10 <= I10 and S0_not and S1 and S2_not and S3 after 4 ns;
+    and11 <= I11 and S0 and S1 and S2_not and S3 after 4 ns;
+    and12 <= I12 and S0_not and S1_not and S2 and S3 after 4 ns;
+    and13 <= I13 and S0 and S1_not and S2 and S3 after 4 ns;
+    and14 <= I14 and S0_not and S1 and S2 and S3 after 4 ns;
+    and15 <= I15 and S0 and S1 and S2 and S3 after 4 ns;
+
+    -- OR the AND gates to produce the final output
+    Y <= and0 or and1 or and2 or and3 or and4 or and5 or and6 or and7 or 
+         and8 or and9 or and10 or and11 or and12 or and13 or and14 or and15 after 2 ns;
+end Behavioral;
 
