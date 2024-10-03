2d8a01cef0d53e60584a2f5a39fbfa786a38db37
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-03 20:59:17 +0100

    Update RF_Mux16_1Bit.vhd

--- src/Register_File/RF_Mux16_1Bit/RF_Mux16_1Bit.vhd
+++ src/Register_File/RF_Mux16_1Bit/RF_Mux16_1Bit.vhd
@@ -21,19 +21,19 @@
 library IEEE ;
 use IEEE . STD_LOGIC_1164 .ALL;
 
-entity Mux16_1Bit is
+entity RF_Mux16_1Bit is
 	Port ( I0 , I1 , I2, I3, I4, I5, I6, I7 : in STD_LOGIC;     -- Input Signals
     I8, I9, I10, I11, I12, I13, I14, I15:  in STD_LOGIC ; 		
 		S0, S1, S2, S3, S4 : in STD_LOGIC; 		            	      -- Selection Signals (2^4 = 16) 
 		Y : out STD_LOGIC ) ;  			                              -- 1 bit output
-end Mux16_1Bit;
+end RF_Mux16_1Bit;
 
-architecture Behavioral of Mux16_1Bit is
+architecture Behavioral of RF_Mux16_1Bit is
 	-- Internal signals to handle the inverted select lines
 	signal S0_not, S1_not, S2_not, S3_not : std_logic;
 	-- Intermediate signals for AND gates
 	signal and0, and1, and2, and3, and4, and5, and6, and7, and8 : std_logic;              
-	signal and8, and9, and10, and11, and12, and13, and14, and15 : std_logic;
+	signal and9, and10, and11, and12, and13, and14, and15 : std_logic;
 begin
 	-- Invert the selection signals
 	S0_not <= not S0 after 3 ns;			-- Might have to change this here for student numbers...
@@ -60,5 +60,5 @@ begin
 
     	-- OR the AND gates to produce the final output
 	Y <= and0 or and1 or and2 or and3 or and4 or and5 or and6 or and7 
-		or and8 or and9 or and10 or and11 or and12 or and13 or and14 or and15 or and16 after 2 ns;
+		or and8 or and9 or and10 or and11 or and12 or and13 or and14 or and15 after 2 ns;
 end Behavioral;
