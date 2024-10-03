9d587cf0b04a341be200096d6ade33e5894b30f9
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-03 13:14:15 +0100

    Update RF_Mux3_1Bit_TB.vhd

--- src/Register_File/RF_Mux3_1Bit/RF_Mux3_1Bit_TB.vhd
+++ src/Register_File/RF_Mux3_1Bit/RF_Mux3_1Bit_TB.vhd
@@ -27,14 +27,19 @@ end Mux3_1Bit_TB;
 architecture Simulation of Mux3_1Bit_TB is
    -- Component Declaration for the Unit Under Test (UUT)
     COMPONENT Mux3_1Bit
-	   Port ( I0 , I1 , I2 :  in STD_LOGIC ; 		-- 1 bit inputs
-   		S0, S1 : in STD_LOGIC; 			-- Selection Signals
-		   Y : out STD_LOGIC ) ;  			-- 1 bit output
-    END COMPONENT;
+	Port ( 
+		I0 : in STD_LOGIC;
+		I1 : in STD_LOGIC;
+		I2 : in STD_LOGIC;
+		S0 : in STD_LOGIC;
+		S1 : in STD_LOGIC;
+		Y  : out STD_LOGIC
+	);
+ END COMPONENT;
 
-   signal S0_not, S1_not : std_logic := '0';
-    -- Intermediate signals for AND gates
-   signal and0, and1, and2 : std_logic := '0';
+   signal I0_TB, I1_TB, I2_TB : STD_LOGIC := '0'; -- inputs for the MUX
+   signal S0_TB, S1_TB : STD_LOGIC := '0'; -- select lines for the MUX
+   signal Y_TB : STD_LOGIC; -- output for the MUX
 
    -- StudentID e.g. 26 33 57 25(DEC) = 1 91 D9 ED(HEX)
    constant StudentID : STD_LOGIC_VECTOR (27 downto 0) := x"154D29D";
@@ -44,33 +49,45 @@ begin
 -- change from here on out
    -- Instantiate the Unit Under Test (UUT)
    uut: Mux3_1Bit_TB PORT MAP (
+	I0 => I0_TB,
+	I1 => I1_TB,
+	I2 => I2_TB,
    	S0 => S0_TB,
-	   S1 : in STD_LOGIC; 		
-		   Y : out STD_LOGIC ) ;  	
-          CLK => CLK_TB,
-          D => D_TB,
-          Q => Q_TB, 
-          Q_not => Q_not_TB
-        );
+	S1 => S1_TB,
+	Y => Y_TB );
         
-   Clk_TB <= not CLK_TB after PERIOD/2;
-   
    stim_proc: process
    begin
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '0' after PERIOD/4;                 -- Case A
-   
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '0' after PERIOD/4;                 -- Case B
+	-- Test case 1: S1 = 0, S0 = 0, select I0
+	I0_TB <= '1';  -- Set I0 to '1'
+	I1_TB <= '0';  -- I1 -> don't care
+	I2_TB <= '0';  -- I2 -> don't care
+	S1_TB <= '0';
+	S0_TB <= '0';
+	wait for PERIOD;  
+	
+	-- Test case 2: S1 = 0, S0 = 1, select I1
+	I0_TB <= '0';
+	I1_TB <= '1';  -- Set I1 to '1'
+	I2_TB <= '0';  -- I2 -> don't care
+	S1_TB <= '0';
+	S0_TB <= '1';
+	wait for PERIOD;
+	
+	-- Test case 3: S1 = 1, S0 = 0, select I2
+	I0_TB <= '0';
+	I1_TB <= '0';
+	I2_TB <= '1';  -- Set I2 to '1'
+	S1_TB <= '1';
+	S0_TB <= '0';
+	wait for PERIOD;
+	
+	-- Test case 4: Invalid state (e.g., S1 = 1, S0 = 1)
+	S1_TB <= '1';
+	S0_TB <= '1';
+	wait for PERIOD;
       
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '1' after PERIOD/4;                 -- Case C
-      
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '0' after PERIOD/4;                 -- Case D
-      
-      wait until CLK_TB'event and CLK_TB='1';
-      D_TB <= '1' after PERIOD/4;                 -- Case E
-      
-      wait until CLK_TB'event and CLK_TB='1';        
+	wait;
+
+	end process; 
 end Simulation;
