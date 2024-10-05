ae79e64f9d1d8453c26da000b957a2b117ce0674
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-05 14:27:35 +0100

    Update RF_Mux32_1Bit_TB.vhd

--- src/Register_File/RF_Mux32_1Bit/RF_Mux32_1Bit_TB.vhd
+++ src/Register_File/RF_Mux32_1Bit/RF_Mux32_1Bit_TB.vhd
@@ -58,7 +58,11 @@ begin
         I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
         I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
         I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
-        S0_TB <= '0';   S1_TB <= '0';   S2_TB <= '0';   S3_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '0';   S2_TB <= '0';   S3_TB <= '0'; S4_TB <= '0'
         wait for PERIOD;
         assert (Y_TB = I0_TB) report "Test Case 1 Failed: I0 was not selected!" severity error;
 
@@ -196,6 +200,218 @@ begin
         S0_TB <= '1';   S1_TB <= '1';   S2_TB <= '1';   S3_TB <= '1';
         wait for PERIOD;
         assert (Y_TB = I15_TB) report "Test Case 16 Failed: I15 was not selected!" severity error;
+
+
+
+
+        -- Test Case 17: 
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '1';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '0';   S2_TB <= '0';   S3_TB <= '0'; S4_TB <= '1'
+        wait for PERIOD;
+        assert (Y_TB = I0_TB) report "Test Case 1 Failed: I0 was not selected!" severity error;
+
+        -- Test Case 2: Select I1 (S0 = 1, S1 = 0, S2 = 0, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '1';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '0';   S2_TB <= '0';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I1_TB) report "Test Case 2 Failed: I1 was not selected!" severity error;
+
+        -- Test Case 3: Select I2 (S0 = 0, S1 = 1, S2 = 0, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '1';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '1';   S2_TB <= '0';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I2_TB) report "Test Case 3 Failed: I2 was not selected!" severity error;
+
+        -- Test Case 4: Select I3 (S0 = 1, S1 = 1, S2 = 0, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '1'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '1';   S2_TB <= '0';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I3_TB) report "Test Case 4 Failed: I3 was not selected!" severity error;
+
+        -- Test Case 5: Select I4 (S0 = 0, S1 = 0, S2 = 1, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '1';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '0';   S2_TB <= '1';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I4_TB) report "Test Case 5 Failed: I4 was not selected!" severity error;
+
+        -- Test Case 6: Select I5 (S0 = 1, S1 = 0, S2 = 1, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '1';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '0';   S2_TB <= '1';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I5_TB) report "Test Case 6 Failed: I5 was not selected!" severity error;
+
+        -- Test Case 7: Select I6 (S0 = 0, S1 = 1, S2 = 1, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '1';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '1';   S2_TB <= '1';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I6_TB) report "Test Case 7 Failed: I6 was not selected!" severity error;
+
+        -- Test Case 8: Select I7 (S0 = 1, S1 = 1, S2 = 1, S3 = 0)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '1';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I27_TB <= '0' ; I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '1';   S2_TB <= '1';   S3_TB <= '0';
+        wait for PERIOD;
+        assert (Y_TB = I7_TB) report "Test Case 8 Failed: I7 was not selected!" severity error;
+
+        -- Test Case 9: Select I8 (S0 = 0, S1 = 0, S2 = 0, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '1';  I25_TB <= '0';  I26_TB <= '0';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '0';   S2_TB <= '0';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I8_TB) report "Test Case 9 Failed: I8 was not selected!" severity error;
+
+        -- Test Case 10: Select I9 (S0 = 1, S1 = 0, S2 = 0, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '1';  I26_TB <= '0';  I27_TB <= '0'; I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '0';   S2_TB <= '0';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I9_TB) report "Test Case 10 Failed: I9 was not selected!" severity error;
+
+        -- Test Case 11: Select I10 (S0 = 0, S1 = 1, S2 = 0, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '1';  I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '1';   S2_TB <= '0';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I10_TB) report "Test Case 11 Failed: I10 was not selected!" severity error;
+
+        -- Test Case 12: Select I11 (S0 = 1, S1 = 1, S2 = 0, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I27_TB <= '1'; I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '1';   S2_TB <= '0';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I11_TB) report "Test Case 12 Failed: I11 was not selected!" severity error;
+
+        -- Test Case 13: Select I12 (S0 = 0, S1 = 0, S2 = 1, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I27_TB <= '0'; I28_TB <= '1';
+        I29_TB <= '0';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '0';   S2_TB <= '1';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I12_TB) report "Test Case 13 Failed: I12 was not selected!" severity error;
+
+        -- Test Case 14: Select I13 (S0 = 1, S1 = 0, S2 = 1, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I27_TB <= '0'; I28_TB <= '0';
+        I29_TB <= '1';  I30_TB <= '0';  I31_TB <= '0';
+        S0_TB <= '1';   S1_TB <= '0';   S2_TB <= '1';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I13_TB) report "Test Case 14 Failed: I13 was not selected!" severity error;
+
+        -- Test Case 15: Select I14 (S0 = 0, S1 = 1, S2 = 1, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I27_TB <= '0'; I28_TB <= '0';
+        I29_TB <= '0';  I30_TB <= '1';  I31_TB <= '0';
+        S0_TB <= '0';   S1_TB <= '1';   S2_TB <= '1';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I14_TB) report "Test Case 15 Failed: I14 was not selected!" severity error;
+
+        -- Test Case 16: Select I15 (S0 = 1, S1 = 1, S2 = 1, S3 = 1)
+        I0_TB <= '0';   I1_TB <= '0';   I2_TB <= '0';   I3_TB <= '0'; 
+        I4_TB <= '0';   I5_TB <= '0';   I6_TB <= '0';   I7_TB <= '0';
+        I8_TB <= '0';   I9_TB <= '0';   I10_TB <= '0';  I11_TB <= '0';
+        I12_TB <= '0';  I13_TB <= '0';  I14_TB <= '0';  I15_TB <= '0';
+        I16_TB <= '0';  I17_TB <= '0';  I18_TB <= '0';  I19_TB <= '0'; 
+        I20_TB <= '0';  I21_TB <= '0';  I22_TB <= '0';  I23_TB <= '0';
+        I24_TB <= '0';  I25_TB <= '0';  I26_TB <= '0';  I27_TB <= '0'; I28_TB <= '0';
+        I29_TB <= 01';  I30_TB <= '0';  I31_TB <= '1';
+        S0_TB <= '1';   S1_TB <= '1';   S2_TB <= '1';   S3_TB <= '1';
+        wait for PERIOD;
+        assert (Y_TB = I15_TB) report "Test Case 16 Failed: I15 was not selected!" severity error;
+
 
         -- End of test cases
         wait;  -- Stop simulation
