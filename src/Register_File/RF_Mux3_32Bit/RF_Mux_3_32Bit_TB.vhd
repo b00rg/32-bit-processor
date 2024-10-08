37acd56b2a3a1caa4ee4d0d2efc414cf8ead4d14
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-08 10:55:57 +0100

    Update RF_Mux_3_32Bit_TB.vhd

--- src/Register_File/RF_Mux3_32Bit/RF_Mux_3_32Bit_TB.vhd
+++ src/Register_File/RF_Mux3_32Bit/RF_Mux_3_32Bit_TB.vhd
@@ -35,9 +35,9 @@ architecture simulation of RF_Mux3_32Bit_TB is
              );                                                                                                                                                         
     end component;                                                                                                                                                      
         -- Signals for inputs and outputs                                                                                                                               
-    signal I0_TB, I1_TB, I2_TB : STD_LOGIC_VECTOR(31 downto 0);                                                                                                         
+    signal I0_TB, I1_TB, I2_TB : STD_LOGIC_VECTOR(31 downto 0) => (others => '0');                                                                                                         
     signal S0_TB, S1_TB : STD_LOGIC := '0';                                                                                                                             
-    signal Y_TB : STD_LOGIC_VECTOR(31 downto 0);                                                                                                                        
+    signal Y_TB : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');                                                                                                                        
                                                                                                                                                                         
 begin                                                                                                                                                                   
     -- Instantiate the Unit Under Test (UUT)                                                                                                                            
@@ -56,16 +56,18 @@ begin
         S0_TB <= '0';                                                                                                                                                   
         S1_TB <= '0';                                                                                                                                                   
         -- Test case 1: Select I0 (S0 = '0', S1 = '0')                                                                                                                  
-        I0_TB <= "00000000000000000000000000000001";  -- 32-bit value                                                                                                   
-        I1_TB <= "00000000000000000000000000000010";  -- 32-bit value                                                                                                   
-        I2_TB <= "00000000000000000000000000000100";  -- 32-bit value                                                                                                   
-                                                                                                                                                                        
-                                                                                                                                                                        
-        wait for 100 ns;                                                                                                                                                
+        I0_TB <= "00000000000000000000000000000000";  -- 32-bit value                                                                                                   
+        I1_TB <= "00000000000000000000000000000000";  -- 32-bit value                                                                                                   
+        I2_TB <= "00000000000000000000000000000000";  -- 32-bit value                                                                                                   
+                                                                                                                                                                                                                                                                                                                           
+        wait for 60 ns;                                                                                                                                                
                                                                                                                                                                         
         -- Test case 2: Select I1 (S0 = '1', S1 = '0')                                                                                                                  
         S0_TB <= '1';                                                                                                                                                   
-        S1_TB <= '0';                                                                                                                                                   
+        S1_TB <= '0';  
+        I0_TB <= "00000000000000000000000000000001";  -- 32-bit value                                                                                                   
+        I1_TB <= "00000000000000000000000000000000";  -- 32-bit value                                                                                                   
+        I2_TB <= "00000000000000000000000000000000";  -- 32-bit value      
         wait for 100 ns;                                                                                                                                                
                                                                                                                                                                         
         -- Test case 3: Select I2 (S0 = '0', S1 = '1')                                                                                                                  
