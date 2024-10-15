614872d60568f0041ad5b5085835953e337770aa
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-15 16:48:41 +0100

    Update RF_RegisterFile_32_15_22336157_TB.vhd

--- src/Register_File/RF_RegisterFile_32_15/RF_RegisterFile_32_15_22336157_TB.vhd
+++ src/Register_File/RF_RegisterFile_32_15/RF_RegisterFile_32_15_22336157_TB.vhd
@@ -24,53 +24,53 @@ ARCHITECTURE simulation OF RF_RegisterFile_32_15_22336157_TB IS
     END COMPONENT;
 
     -- Signals Declaration
-    signal CLK : std_logic := '0';
-    signal RW : std_logic := '0';                     -- Read/Write signal
-    signal Reset : std_logic := '0';                  -- Reset signal
-    signal DR : std_logic_vector(4 downto 0) := (others => '0'); -- 5-bit destination register address
-    signal D : std_logic_vector(31 downto 0) := (others => '0'); -- 32-bit data input
-    signal sa, sb : std_logic_vector(4 downto 0) := (others => '0'); 
-    signal ta, td, tb : std_logic_vector(3 downto 0) := (others => '0'); 
-    signal a, b : std_logic_vector(31 downto 0) := (others => '0');  -- Selection/control inputs
+    signal CLK_TB : std_logic := '0';
+    signal RW_TB : std_logic := '0';                     -- Read/Write signal
+    signal Reset_TB : std_logic := '0';                  -- Reset signal
+    signal DR_TB : std_logic_vector(4 downto 0) := (others => '0'); -- 5-bit destination register address
+    signal D_TB : std_logic_vector(31 downto 0) := (others => '0'); -- 32-bit data input
+    signal sa_TB, sb_TB : std_logic_vector(4 downto 0) := (others => '0'); 
+    signal ta_TB, td_TB, tb_TB : std_logic_vector(3 downto 0) := (others => '0'); 
+    signal a_TB, b_TB : std_logic_vector(31 downto 0) := (others => '0');  -- Selection/control inputs
 
     -- Clock period definition
-    constant clk_period : time := 10 ns;
+    constant clk_period : time := 400 ns;
 
 BEGIN
 
     -- Clock generation without a process
-    CLK <= not CLK after clk_period / 2;
+    CLK_TB <= not CLK_TB after clk_period / 2;
 
     -- Instantiate the Unit Under Test (UUT)
     uut: RF_RegisterFile_32_15_22336157 PORT MAP (
-          RW    => RW,
-          DR    => DR,
-          D     => D,
-          CLK   => CLK,
-          Reset => Reset,
-          sa    => sa,
-          sb    => sb,
-          ta    => ta,
-          tb    => tb, 
-          td    => td,
-          a     => a,
-          b     => b 
+          RW    => RW_TB,
+          DR    => DR_TB,
+          D     => D_TB,
+          CLK   => CLK_TB,
+          Reset => Reset_TB,
+          sa    => sa_TB,
+          sb    => sb_TB,
+          ta    => ta_TB,
+          tb    => tb_TB, 
+          td    => td_TB,
+          a     => a_TB,
+          b     => b_TB 
     );
 
     -- Stimulus process
     stim_proc: process
     begin	
-        reset <= '1';
-        wait for clk_period*2; 
-        -- Test Case 1: register 0
-        Reset <= '0';
-        wait for clk_period*2; 
-        RW <= '0';  -- Enable write
-        dr <= "00000"; 
-        td <= "0000";       
-        D <= "00000000000000000000000000000010";  -- Write 1 to register 0
+        reset_TB <= '1';
         wait for clk_period;
-
+        -- Test Case 1: register 0
+        Reset_TB <= '0';
+        wait for clk_period; 
+        RW_TB <= '0';  -- Enable write
+        dr_TB <= "00000"; 
+        td_TB <= "0000";       
+        D_TB <= x"00000001"; 
+        wait for clk_period;
+        RW_TB  <= '1';
         wait;
     end process;
 
