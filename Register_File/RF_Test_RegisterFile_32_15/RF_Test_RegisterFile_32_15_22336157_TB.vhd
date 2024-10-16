6d87d210562bdd8990ab079ffd7efa950435a784
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-16 15:51:17 +0100

    Update RF_Test_RegisterFile_32_15_22336157_TB.vhd

--- Register_File/RF_Test_RegisterFile_32_15/RF_Test_RegisterFile_32_15_22336157_TB.vhd
+++ Register_File/RF_Test_RegisterFile_32_15/RF_Test_RegisterFile_32_15_22336157_TB.vhd
@@ -259,6 +259,231 @@ BEGIN
     dr_Test <= "31";
     wait for clk_period;
 
+        A_B_DataIN_Test <= "00";
+-- Test Case 32A: select input from port A, source register 0
+    SA_Test <= "00000";
+    wait for CLK_period;
+-- Test Case 32B: select input from port B, source register 0
+    SB_Test <= "00000";
+    wait for clk_period;
+
+-- Test Case 33A: select input from port A, source register 1
+    SA_Test <= "00001";
+    wait for CLK_period;
+-- Test Case 33B: select input from port B, source register 1
+    SB_Test <= "00001";
+    wait for clk_period;
+
+-- Test Case 34A: select input from port A, source register 2
+    SA_Test <= "00010";
+    wait for CLK_period;
+-- Test Case 34B: select input from port B, source register 2
+    SB_Test <= "00010";
+    wait for clk_period;
+
+-- Test Case 35A: select input from port A, source register 3
+    SA_Test <= "00011";
+    wait for CLK_period;
+-- Test Case 35B: select input from port B, source register 3
+    SB_Test <= "00011";
+    wait for clk_period;
+
+-- Test Case 36A: select input from port A, source register 4
+    SA_Test <= "00100";
+    wait for CLK_period;
+-- Test Case 36B: select input from port B, source register 4
+    SB_Test <= "00100";
+    wait for clk_period;
+
+-- Test Case 37A: select input from port A, source register 5
+    SA_Test <= "00101";
+    wait for CLK_period;
+-- Test Case 37B: select input from port B, source register 5
+    SB_Test <= "00101";
+    wait for clk_period;
+
+-- Test Case 38A: select input from port A, source register 6
+    SA_Test <= "00110";
+    wait for CLK_period;
+-- Test Case 38B: select input from port B, source register 6
+    SB_Test <= "00110";
+    wait for clk_period;
+
+-- Test Case 39A: select input from port A, source register 7
+    SA_Test <= "00111";
+    wait for CLK_period;
+-- Test Case 39B: select input from port B, source register 7
+    SB_Test <= "00111";
+    wait for clk_period;
+
+-- Test Case 40A: select input from port A, source register 8
+    SA_Test <= "01000";
+    wait for CLK_period;
+-- Test Case 40B: select input from port B, source register 8
+    SB_Test <= "01000";
+    wait for clk_period;
+
+-- Test Case 41A: select input from port A, source register 9
+    SA_Test <= "01001";
+    wait for CLK_period;
+-- Test Case 41B: select input from port B, source register 9
+    SB_Test <= "01001";
+    wait for clk_period;
+
+-- Test Case 42A: select input from port A, source register 10
+    SA_Test <= "01010";
+    wait for CLK_period;
+-- Test Case 42B: select input from port B, source register 10
+    SB_Test <= "01010";
+    wait for clk_period;
+
+-- Test Case 43A: select input from port A, source register 11
+    SA_Test <= "01011";
+    wait for CLK_period;
+-- Test Case 43B: select input from port B, source register 11
+    SB_Test <= "01011";
+    wait for clk_period;
+
+-- Test Case 44A: select input from port A, source register 12
+    SA_Test <= "01100";
+    wait for CLK_period;
+-- Test Case 44B: select input from port B, source register 12
+    SB_Test <= "01100";
+    wait for clk_period;
+
+-- Test Case 45A: select input from port A, source register 13
+    SA_Test <= "01101";
+    wait for CLK_period;
+-- Test Case 45B: select input from port B, source register 13
+    SB_Test <= "01101";
+    wait for clk_period;
+
+-- Test Case 46A: select input from port A, source register 14
+    SA_Test <= "01110";
+    wait for CLK_period;
+-- Test Case 46B: select input from port B, source register 14
+    SB_Test <= "01110";
+    wait for clk_period;
+
+-- Test Case 47A: select input from port A, source register 15
+    SA_Test <= "01111";
+    wait for CLK_period;
+-- Test Case 47B: select input from port B, source register 15
+    SB_Test <= "01111";
+    wait for clk_period;
+
+-- Test Case 48A: select input from port A, source register 16
+    SA_Test <= "10000";
+    wait for CLK_period;
+-- Test Case 48B: select input from port B, source register 16
+    SB_Test <= "10000";
+    wait for clk_period;
+
+-- Test Case 49A: select input from port A, source register 17
+    SA_Test <= "10001";
+    wait for CLK_period;
+-- Test Case 49B: select input from port B, source register 17
+    SB_Test <= "10001";
+    wait for clk_period;
+
+-- Test Case 50A: select input from port A, source register 18
+    SA_Test <= "10010";
+    wait for CLK_period;
+-- Test Case 50B: select input from port B, source register 18
+    SB_Test <= "10010";
+    wait for clk_period;
+
+-- Test Case 51A: select input from port A, source register 19
+    SA_Test <= "10011";
+    wait for CLK_period;
+-- Test Case 51B: select input from port B, source register 19
+    SB_Test <= "10011";
+    wait for clk_period;
+
+-- Test Case 52A: select input from port A, source register 20
+    SA_Test <= "10100";
+    wait for CLK_period;
+-- Test Case 52B: select input from port B, source register 20
+    SB_Test <= "10100";
+    wait for clk_period;
+
+-- Test Case 53A: select input from port A, source register 21
+    SA_Test <= "10101";
+    wait for CLK_period;
+-- Test Case 53B: select input from port B, source register 21
+    SB_Test <= "10101";
+    wait for clk_period;
+
+-- Test Case 54A: select input from port A, source register 22
+    SA_Test <= "10110";
+    wait for CLK_period;
+-- Test Case 54B: select input from port B, source register 22
+    SB_Test <= "10110";
+    wait for clk_period;
+
+-- Test Case 55A: select input from port A, source register 23
+    SA_Test <= "10111";
+    wait for CLK_period;
+-- Test Case 55B: select input from port B, source register 23
+    SB_Test <= "10111";
+    wait for clk_period;
+
+-- Test Case 56A: select input from port A, source register 24
+    SA_Test <= "11000";
+    wait for CLK_period;
+-- Test Case 56B: select input from port B, source register 24
+    SB_Test <= "11000";
+    wait for clk_period;
+
+-- Test Case 57A: select input from port A, source register 25
+    SA_Test <= "11001";
+    wait for CLK_period;
+-- Test Case 57B: select input from port B, source register 25
+    SB_Test <= "11001";
+    wait for clk_period;
+
+-- Test Case 58A: select input from port A, source register 26
+    SA_Test <= "11010";
+    wait for CLK_period;
+-- Test Case 58B: select input from port B, source register 26
+    SB_Test <= "11010";
+    wait for clk_period;
+
+-- Test Case 59A: select input from port A, source register 27
+    SA_Test <= "11011";
+    wait for CLK_period;
+-- Test Case 59B: select input from port B, source register 27
+    SB_Test <= "11011";
+    wait for clk_period;
+
+-- Test Case 60A: select input from port A, source register 28
+    SA_Test <= "11100";
+    wait for CLK_period;
+-- Test Case 60B: select input from port B, source register 28
+    SB_Test <= "11100";
+    wait for clk_period;
+
+-- Test Case 61A: select input from port A, source register 29
+    SA_Test <= "11101";
+    wait for CLK_period;
+-- Test Case 61B: select input from port B, source register 29
+    SB_Test <= "11101";
+    wait for clk_period;
+
+-- Test Case 62A: select input from port A, source register 30
+    SA_Test <= "11110";
+    wait for CLK_period;
+-- Test Case 62B: select input from port B, source register 30
+    SB_Test <= "11110";
+    wait for clk_period;
+
+-- Test Case 63A: select input from port A, source register 31
+    SA_Test <= "11111";
+    wait for CLK_period;
+-- Test Case 63B: select input from port B, source register 31
+    SB_Test <= "11111";
+    wait for clk_period;
+
         -- Test completed
         wait;
     end process;
