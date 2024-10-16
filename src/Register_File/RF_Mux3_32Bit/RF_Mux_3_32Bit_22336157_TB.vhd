ad694f0ce8db8bd09b41b2ef9d26a4b6222fa556
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-16 10:14:13 +0100

    Update RF_Mux_3_32Bit_22336157_TB.vhd

--- src/Register_File/RF_Mux3_32Bit/RF_Mux_3_32Bit_22336157_TB.vhd
+++ src/Register_File/RF_Mux3_32Bit/RF_Mux_3_32Bit_22336157_TB.vhd
@@ -1,22 +1,23 @@
-----------------------------------------------------------------------------------                                                                                      
--- Company:                                                                                                                                                             
--- Engineer:                                                                                                                                                            
---                                                                                                                                                                      
--- Create Date: 20.09.2023 14:53:59                                                                                                                                     
--- Design Name:                                                                                                                                                         
--- Module Name: Mux3_32Bit - Behavioral                                                                                                                                 
--- Project Name:                                                                                                                                                        
--- Target Devices:                                                                                                                                                      
--- Tool Versions:                                                                                                                                                       
--- Description:                                                                                                                                                         
---                                                                                                                                                                      
--- Dependencies:                                                                                                                                                        
---                                                                                                                                                                      
--- Revision:                                                                                                                                                            
--- Revision 0.01 - File Created                                                                                                                                         
--- Additional Comments:                                                                                                                                                 
---                                                                                                                                                                      
-----------------------------------------------------------------------------------                                                                                      
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Emma Burgess
+-- 
+-- Create Date: 20.09.2023 14:53:59
+-- Design Name: 
+-- Module Name: Mux3_32Bit_TB - Simulation
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+                                                                                 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
