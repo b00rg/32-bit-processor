c8ea6013c279886e9a1db6d1f3f33addcf74e173
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-22 14:18:16 +0100

    Update DP_32Bit_B_Logic_22336157.vhd

--- Functional_Unit/DP_32Bit_B_Logic/DP_32Bit_B_Logic_22336157.vhd
+++ Functional_Unit/DP_32Bit_B_Logic/DP_32Bit_B_Logic_22336157.vhd
@@ -1,1 +1,22 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Emma Burgess
+-- 
+-- Create Date: 06.10.2022 22:19:50
+-- Design Name: 
+-- Module Name: 32Bit_B_Logic_22336157 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 
+begin 
+   BIT00: RF_Mux32_1Bit_22336157 Port map (I0 => I0(0), I1
