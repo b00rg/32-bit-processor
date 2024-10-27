9033dc40af0b62ddefa142e012f5262f044259dc
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-27 14:02:53 +0000

    Update DP_RippleCarryAdder32Bit_22336157.vhd

--- Functional_Unit/DP_RippleCarryAdder32Bit/DP_RippleCarryAdder32Bit_22336157.vhd
+++ Functional_Unit/DP_RippleCarryAdder32Bit/DP_RippleCarryAdder32Bit_22336157.vhd
@@ -10,7 +10,7 @@
 -- Tool Versions: 
 -- Description: 32-bit Ripple Carry Adder
 -- 
--- Dependencies: FullAdder_22336157.vhdl
+-- Dependencies: DP_FullAdder_22336157.vhdl
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
@@ -21,7 +21,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity RippleCarryAdder32Bit_22336157 is
+entity DP_RippleCarryAdder32Bit_22336157 is
     Port ( 
         A : in STD_LOGIC_VECTOR (31 downto 0);
         B : in STD_LOGIC_VECTOR (31 downto 0);
@@ -30,11 +30,11 @@ entity RippleCarryAdder32Bit_22336157 is
         C_OUT : out STD_LOGIC; -- final carry-out bit
         V : out STD_LOGIC -- overflow indicator
     );
-end RippleCarryAdder32Bit_22336157;
+end DP_RippleCarryAdder32Bit_22336157;
 
-architecture Behavioral of RippleCarryAdder32Bit_22336157 is
+architecture Behavioral of DP_RippleCarryAdder32Bit_22336157 is
 
-    COMPONENT FullAdder_22336157
+    COMPONENT DP_DP_FullAdder_22336157
         PORT(
             A : in STD_LOGIC;
             B : in STD_LOGIC;
@@ -60,7 +60,7 @@ begin
 
 
     -- First bit (Bit 0)
-    BIT0: FullAdder_22336157 PORT MAP (
+    BIT0: DP_FullAdder_22336157 PORT MAP (
             A => A(0),
             B => B(0),
             C_IN => C_IN, -- initial carry-in
@@ -69,7 +69,7 @@ begin
         );
 
     -- Second bit (Bit 1)
-    BIT1: FullAdder_22336157 PORT MAP (
+    BIT1: DP_FullAdder_22336157 PORT MAP (
             A => A(1),
             B => B(1),
             C_IN => carry(1),
@@ -78,7 +78,7 @@ begin
         );
 
     -- Third bit (Bit 2)
-    BIT2: FullAdder_22336157 PORT MAP (
+    BIT2: DP_FullAdder_22336157 PORT MAP (
             A => A(2),
             B => B(2),
             C_IN => carry(2),
@@ -87,7 +87,7 @@ begin
         );
 
     -- Fourth bit (Bit 3)
-    BIT3: FullAdder_22336157 PORT MAP (
+    BIT3: DP_FullAdder_22336157 PORT MAP (
             A => A(3),
             B => B(3),
             C_IN => carry(3),
@@ -96,7 +96,7 @@ begin
         );
 
     -- Fifth bit (Bit 4)
-    BIT4: FullAdder_22336157 PORT MAP (
+    BIT4: DP_FullAdder_22336157 PORT MAP (
             A => A(4),
             B => B(4),
             C_IN => carry(4),
@@ -105,7 +105,7 @@ begin
         );
 
     -- Sixth bit (Bit 5)
-    BIT5: FullAdder_22336157 PORT MAP (
+    BIT5: DP_FullAdder_22336157 PORT MAP (
             A => A(5),
             B => B(5),
             C_IN => carry(5),
@@ -114,7 +114,7 @@ begin
         );
 
     -- Seventh bit (Bit 6)
-    BIT6: FullAdder_22336157 PORT MAP (
+    BIT6: DP_FullAdder_22336157 PORT MAP (
             A => A(6),
             B => B(6),
             C_IN => carry(6),
@@ -123,7 +123,7 @@ begin
         );
 
     -- Eighth bit (Bit 7)
-    BIT7: FullAdder_22336157 PORT MAP (
+    BIT7: DP_FullAdder_22336157 PORT MAP (
             A => A(7),
             B => B(7),
             C_IN => carry(7),
@@ -132,7 +132,7 @@ begin
         );
 
     -- Ninth bit (Bit 8)
-    BIT8: FullAdder_22336157 PORT MAP (
+    BIT8: DP_FullAdder_22336157 PORT MAP (
             A => A(8),
             B => B(8),
             C_IN => carry(8),
@@ -141,7 +141,7 @@ begin
         );
 
     -- Tenth bit (Bit 9)
-    BIT9: FullAdder_22336157 PORT MAP (
+    BIT9: DP_FullAdder_22336157 PORT MAP (
             A => A(9),
             B => B(9),
             C_IN => carry(9),
@@ -150,7 +150,7 @@ begin
         );
 
     -- Eleventh bit (Bit 10)
-    BIT10: FullAdder_22336157 PORT MAP (
+    BIT10: DP_FullAdder_22336157 PORT MAP (
             A => A(10),
             B => B(10),
             C_IN => carry(10),
@@ -159,7 +159,7 @@ begin
         );
 
     -- Twelfth bit (Bit 11)
-    BIT11: FullAdder_22336157 PORT MAP (
+    BIT11: DP_FullAdder_22336157 PORT MAP (
             A => A(11),
             B => B(11),
             C_IN => carry(11),
@@ -168,7 +168,7 @@ begin
         );
 
     -- Thirteenth bit (Bit 12)
-    BIT12: FullAdder_22336157 PORT MAP (
+    BIT12: DP_FullAdder_22336157 PORT MAP (
             A => A(12),
             B => B(12),
             C_IN => carry(12),
@@ -177,7 +177,7 @@ begin
         );
 
     -- Fourteenth bit (Bit 13)
-    BIT13: FullAdder_22336157 PORT MAP (
+    BIT13: DP_FullAdder_22336157 PORT MAP (
             A => A(13),
             B => B(13),
             C_IN => carry(13),
@@ -186,7 +186,7 @@ begin
         );
 
     -- Fifteenth bit (Bit 14)
-    BIT14: FullAdder_22336157 PORT MAP (
+    BIT14: DP_FullAdder_22336157 PORT MAP (
             A => A(14),
             B => B(14),
             C_IN => carry(14),
@@ -195,7 +195,7 @@ begin
         );
 
     -- Sixteenth bit (Bit 15)
-    BIT15: FullAdder_22336157 PORT MAP (
+    BIT15: DP_FullAdder_22336157 PORT MAP (
             A => A(15),
             B => B(15),
             C_IN => carry(15),
@@ -204,7 +204,7 @@ begin
         );
 
     -- Seventeenth bit (Bit 16)
-    BIT16: FullAdder_22336157 PORT MAP (
+    BIT16: DP_FullAdder_22336157 PORT MAP (
             A => A(16),
             B => B(16),
             C_IN => carry(16),
@@ -213,7 +213,7 @@ begin
         );
 
     -- Eighteenth bit (Bit 17)
-    BIT17: FullAdder_22336157 PORT MAP (
+    BIT17: DP_FullAdder_22336157 PORT MAP (
             A => A(17),
             B => B(17),
             C_IN => carry(17),
@@ -222,7 +222,7 @@ begin
         );
 
     -- Nineteenth bit (Bit 18)
-    BIT18: FullAdder_22336157 PORT MAP (
+    BIT18: DP_FullAdder_22336157 PORT MAP (
             A => A(18),
             B => B(18),
             C_IN => carry(18),
@@ -231,7 +231,7 @@ begin
         );
 
     -- Twentieth bit (Bit 19)
-    BIT19: FullAdder_22336157 PORT MAP (
+    BIT19: DP_FullAdder_22336157 PORT MAP (
             A => A(19),
             B => B(19),
             C_IN => carry(19),
@@ -240,7 +240,7 @@ begin
         );
 
     -- Twenty-first bit (Bit 20)
-    BIT20: FullAdder_22336157 PORT MAP (
+    BIT20: DP_FullAdder_22336157 PORT MAP (
             A => A(20),
             B => B(20),
             C_IN => carry(20),
@@ -249,7 +249,7 @@ begin
         );
 
     -- Twenty-second bit (Bit 21)
-    BIT21: FullAdder_22336157 PORT MAP (
+    BIT21: DP_FullAdder_22336157 PORT MAP (
             A => A(21),
             B => B(21),
             C_IN => carry(21),
@@ -258,7 +258,7 @@ begin
         );
 
     -- Twenty-third bit (Bit 22)
-    BIT22: FullAdder_22336157 PORT MAP (
+    BIT22: DP_FullAdder_22336157 PORT MAP (
             A => A(22),
             B => B(22),
             C_IN => carry(22),
@@ -267,7 +267,7 @@ begin
         );
 
     -- Twenty-fourth bit (Bit 23)
-    BIT23: FullAdder_22336157 PORT MAP (
+    BIT23: DP_FullAdder_22336157 PORT MAP (
             A => A(23),
             B => B(23),
             C_IN => carry(23),
@@ -276,7 +276,7 @@ begin
         );
 
     -- Twenty-fifth bit (Bit 24)
-    BIT24: FullAdder_22336157 PORT MAP (
+    BIT24: DP_FullAdder_22336157 PORT MAP (
             A => A(24),
             B => B(24),
             C_IN => carry(24),
@@ -285,7 +285,7 @@ begin
         );
 
     -- Twenty-sixth bit (Bit 25)
-    BIT25: FullAdder_22336157 PORT MAP (
+    BIT25: DP_FullAdder_22336157 PORT MAP (
             A => A(25),
             B => B(25),
             C_IN => carry(25),
@@ -294,7 +294,7 @@ begin
         );
 
     -- Twenty-seventh bit (Bit 26)
-    BIT26: FullAdder_22336157 PORT MAP (
+    BIT26: DP_FullAdder_22336157 PORT MAP (
             A => A(26),
             B => B(26),
             C_IN => carry(26),
@@ -303,7 +303,7 @@ begin
         );
 
     -- Twenty-eighth bit (Bit 27)
-    BIT27: FullAdder_22336157 PORT MAP (
+    BIT27: DP_FullAdder_22336157 PORT MAP (
             A => A(27),
             B => B(27),
             C_IN => carry(27),
@@ -312,7 +312,7 @@ begin
         );
 
     -- Twenty-ninth bit (Bit 28)
-    BIT28: FullAdder_22336157 PORT MAP (
+    BIT28: DP_FullAdder_22336157 PORT MAP (
             A => A(28),
             B => B(28),
             C_IN => carry(28),
@@ -321,7 +321,7 @@ begin
         );
 
     -- Thirtieth bit (Bit 29)
-    BIT29: FullAdder_22336157 PORT MAP (
+    BIT29: DP_FullAdder_22336157 PORT MAP (
             A => A(29),
             B => B(29),
             C_IN => carry(29),
@@ -330,7 +330,7 @@ begin
         );
 
     -- Thirty-first bit (Bit 30)
-    BIT30: FullAdder_22336157 PORT MAP (
+    BIT30: DP_FullAdder_22336157 PORT MAP (
             A => A(30),
             B => B(30),
             C_IN => carry(30),
@@ -339,7 +339,7 @@ begin
         );
 
     -- Final bit (Bit 31)
-    BIT31: FullAdder_22336157 PORT MAP (
+    BIT31: DP_FullAdder_22336157 PORT MAP (
             A => A(31),
             B => B(31),
             C_IN => carry(31),
