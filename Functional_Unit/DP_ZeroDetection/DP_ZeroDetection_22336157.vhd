e85f8e5b85ed21ff248b185fad9a2bb3f87a145a
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-29 15:07:32 +0000

    Update DP_ZeroDetection_22336157.vhd

--- Functional_Unit/DP_ZeroDetection/DP_ZeroDetection_22336157.vhd
+++ Functional_Unit/DP_ZeroDetection/DP_ZeroDetection_22336157.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity DP_ZeroDetection_22336157 is
     Port (
-        input_vector : in  STD_LOGIC_VECTOR(30 downto 0);  -- 31-bit input vector
+        input_vector : in  STD_LOGIC_VECTOR(31 downto 0);  -- 31-bit input vector
         zero_flag    : out STD_LOGIC                         -- 1-bit output flag
     );
 end DP_ZeroDetection_22336157;
@@ -11,6 +11,6 @@ end DP_ZeroDetection_22336157;
 architecture Simulation of DP_ZeroDetection_22336157 is
 begin
     -- Directly assign zero_flag based on the condition of input_vector
-    zero_flag <= '1' when (input_vector = (others => '0')) else '0';
+    zero_flag <= '1' when input_vector = "0000000000000000000000000000000" else '0'; 
 
 end Simulation;
