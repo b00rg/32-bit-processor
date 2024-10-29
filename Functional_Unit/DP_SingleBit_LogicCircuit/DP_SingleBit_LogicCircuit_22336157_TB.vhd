ad4fc5866be96380ebe9be6780b6207583e8bcdb
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-29 16:26:04 +0000

    Update DP_SingleBit_LogicCircuit_22336157_TB.vhd

--- Functional_Unit/DP_SingleBit_LogicCircuit/DP_SingleBit_LogicCircuit_22336157_TB.vhd
+++ Functional_Unit/DP_SingleBit_LogicCircuit/DP_SingleBit_LogicCircuit_22336157_TB.vhd
@@ -8,26 +8,26 @@ architecture Simulation of DP_SingleBit_Logic_Circuit_22336157_TB is
 
     -- Component declaration for the DUT (Device Under Test)
     component DP_SingleBit_Logic_Circuit_22336157
-	Port ( A , B , C :  in STD_LOGIC ; 		-- 1 bit inputs
+	Port ( A , B   :  in STD_LOGIC ; 		-- 1 bit inputs
 		S0, S1 : in STD_LOGIC; 			-- Selection Signals
 		G : out STD_LOGIC ) ;  
     end component;
 
     -- Signals for inputs and output
-    signal A, B, C, S0, S1 : STD_LOGIC;
+    signal A, B, S0, S1 : STD_LOGIC;
     signal G : STD_LOGIC;
 
 begin
 
     -- Instantiate the DUT
     uut: DP_SingleBit_Logic_Circuit_22336157
-	Port map (A => A, B=> B, C => C, S0 => S0, S1 => S1, G => G);  
+	Port map (A => A, B=> B,  S0 => S0, S1 => S1, G => G);  
     -- Test process
     
     process
     begin
         -- Test Case 1: A=0, B=0, c = 1, S0=0, S1=0
-        A <= '0'; B <= '0'; C <= '0'; S0 <= '0'; S1 <= '0';
+        A <= '0'; B <= '0'; S0 <= '0'; S1 <= '0';
         wait for 10 ns;
 
         -- Test Case 2: A=0, B=0, S0=0, S1=1
@@ -87,75 +87,6 @@ begin
         wait for 10 ns;
 
         -- Test Case 16: A=1, B=1, S0=1, S1=1
-        A <= '1'; B <= '1'; S0 <= '1'; S1 <= '1';
-        wait for 10 ns;
-
-
-
-
-
-
-    -- Test Case 17: A=0, B=0, C = 1, S0=0, S1=0
-        A <= '0'; B <= '0'; C <= '1'; S0 <= '0'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 18: A=0, B=0, S0=0, S1=1
-        A <= '0'; B <= '0'; S0 <= '0'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 19: A=0, B=0, S0=1, S1=0
-        A <= '0'; B <= '0'; S0 <= '1'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 20: A=0, B=0, S0=1, S1=1
-        A <= '0'; B <= '0'; S0 <= '1'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 21: A=0, B=1, S0=0, S1=0
-        A <= '0'; B <= '1'; S0 <= '0'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 22: A=0, B=1, S0=0, S1=1
-        A <= '0'; B <= '1'; S0 <= '0'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 23: A=0, B=1, S0=1, S1=0
-        A <= '0'; B <= '1'; S0 <= '1'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 24: A=0, B=1, S0=1, S1=1
-        A <= '0'; B <= '1'; S0 <= '1'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 25: A=1, B=0, S0=0, S1=0
-        A <= '1'; B <= '0'; S0 <= '0'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 26: A=1, B=0, S0=0, S1=1
-        A <= '1'; B <= '0'; S0 <= '0'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 27: A=1, B=0, S0=1, S1=0
-        A <= '1'; B <= '0'; S0 <= '1'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 28: A=1, B=0, S0=1, S1=1
-        A <= '1'; B <= '0'; S0 <= '1'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 29: A=1, B=1, S0=0, S1=0
-        A <= '1'; B <= '1'; S0 <= '0'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 30: A=1, B=1, S0=0, S1=1
-        A <= '1'; B <= '1'; S0 <= '0'; S1 <= '1';
-        wait for 10 ns;
-
-        -- Test Case 31: A=1, B=1, S0=1, S1=0
-        A <= '1'; B <= '1'; S0 <= '1'; S1 <= '0';
-        wait for 10 ns;
-
-        -- Test Case 32: A=1, B=1, S0=1, S1=1
         A <= '1'; B <= '1'; S0 <= '1'; S1 <= '1';
         wait for 10 ns;
         
