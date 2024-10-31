979e2b456257ab9edde284350a3e904d359e35fe
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-31 13:50:54 +0000

    Create DP_FunctionalUnit_22336157.vhd

---
+++ Functional_Unit/DP_FunctionalUnit/DP_FunctionalUnit_22336157.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 31.10.2024 13:47:07
+-- Design Name: 
+-- Module Name: DP_FunctionalUnit_22336157 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DP_FunctionalUnit_22336157 is
+    Port ( B, A : in STD_LOGIC_VECTOR(31 downto 0);
+        FS : in STD_LOGIC_VECTOR(4 downto 0);
+        C, N, Z, V : out STD_LOGIC; 
+        F : out STD_LOGIC_VECTOR(31 downto 0));
+end DP_FunctionalUnit_22336157;
+
+
+
+architecture Behavioral of DP_FunctionalUnit_22336157 is
+
+begin
+
+
+end Behavioral;
