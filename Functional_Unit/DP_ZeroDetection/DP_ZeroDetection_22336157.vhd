9100883c6317a57ac3ce7bb2c8fbcc0a07fef193
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-11-01 10:49:19 +0000

    Update DP_ZeroDetection_22336157.vhd

--- Functional_Unit/DP_ZeroDetection/DP_ZeroDetection_22336157.vhd
+++ Functional_Unit/DP_ZeroDetection/DP_ZeroDetection_22336157.vhd
@@ -29,6 +29,14 @@ entity DP_ZeroDetection_22336157 is
 end DP_ZeroDetection_22336157;
 
 architecture Behavioral of DP_ZeroDetection_22336157 is
+
+       constant AND_gate_delay : Time := 8ns;      -- least significant digit 6 = 5 + 1
+   constant NAND_gate_delay : Time := 6ns;     -- next more significant digit 3 = 2 + 1
+   constant OR_gate_delay : Time := 2ns;       -- next more significant digit 8 = 7 + 1
+   constant NOR_gate_delay : Time := 7ns;      -- next more significant digit 6 = 5 + 1
+   constant XOR_gate_delay : Time := 4ns;      -- next more significant digit 4 = 3 + 1
+   constant XNOR_gate_delay : Time := 4ns;     -- next more significant digit 4 = 3 + 1
+   constant NOT_gate_delay : Time := 3ns;      -- next more significant digit 7 = 6 + 1
 begin
     -- Directly assign zero_flag based on the condition of input_vector
     zero_flag <= '1' when input_vector = "00000000000000000000000000000000" else '0'; 
