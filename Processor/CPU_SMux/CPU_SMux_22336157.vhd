fe5e68cca040915fd49124255add543cccaf9007
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-12-18 19:40:41 +0000

    Update CPU_SMux_22336157.vhd

--- Processor/CPU_SMux/CPU_SMux_22336157.vhd
+++ Processor/CPU_SMux/CPU_SMux_22336157.vhd
@@ -32,16 +32,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity CPU_SMux_22336157 is
-    Port (Zero : in std_logic;
-          Z_Flag : in std_logic;
-          V_Flag : in std_logic;
-          One : in std_logic;
-          C_Flag : in std_logic;
-          N_Flag : in std_logic;
-          C_Flag_Not : in std_logic;
-          Z_Flag_Not : in std_logic;
-          MS : in std_logic_vector(2 downto 0);
-          CAR : out std_logic
+    Port (Zero : in STD_LOGIC;
+          Z_Flag : in STD_LOGIC;
+          V_Flag : in STD_LOGIC;
+          One : in STD_LOGIC;
+          C_Flag : in STD_LOGIC;
+          N_Flag : in STD_LOGIC;
+          C_Flag_Not : in STD_LOGIC;
+          Z_Flag_Not : in STD_LOGIC;
+          MS : in STD_LOGIC_vector(2 downto 0);
+          CAR : out STD_LOGIC
              );
 end CPU_SMux_22336157;
 
@@ -55,9 +55,9 @@ architecture Behavioral of CPU_SMux_22336157 is
    constant XNOR_gate_delay : Time := 4ns;     -- next more significant digit 4 = 3 + 1
    constant NOT_gate_delay : Time := 3ns;      -- next more significant digit 7 = 6 + 1
    
-signal MS0, MS1, MS2, MS3, MS4, MS5, MS6, MS7, MS8, MS9, MS10, MS11, MS12, not_MS_0, not_MS_1,not_MS_2 : std_logic;
-signal Zero_MS4, ZFlag_MS5, VFlag_MS6, One_MS7, CFlag_MS8, NFlag_MS9, NotCFlag_MS10, NotZFLag_MS11: std_logic;
-signal OR0, OR1,OR2, OR3,OR4, OR5,OR6, OR7 : std_logic;
+signal MS_0, MS_1, MS_2, MS_3, MS_4, MS_5, MS_6, MS_7, MS_8, MS_9, MS_10, MS_11, MS_12, not_MS_0, not_MS_1,not_MS_2 : STD_LOGIC;
+signal Zero_MS_4, ZFlag_MS_5, VFlag_MS_6, One_MS_7, CFlag_MS_8, NFlag_MS_9, NotCFlag_MS_10, NotZFLag_MS_11: STD_LOGIC;
+signal OR_0, OR_1,OR_2, OR_3,OR_4, OR_5,OR_6, OR_7 : STD_LOGIC;
 
 begin
 
@@ -65,36 +65,36 @@ begin
     not_MS_1 <= NOT MS(0) after NOT_GATE_DELAY;
     not_MS_2 <= NOT MS(0) after NOT_GATE_DELAY; 
     
-    MS0 <= not_MS_1  AND not_MS_0 after AND_GATE_DELAY;
-    MS1 <= MS(1) AND not_MS_0 after AND_GATE_DELAY;
-    MS2 <= not_MS_1 AND MS(0) after AND_GATE_DELAY;
-    MS3 <= MS(1) AND MS(0) after AND_GATE_DELAY;
-    MS4 <= MS0 AND not_MS_2 after AND_GATE_DELAY;
-    MS5 <= MS0 AND MS(2) after AND_GATE_DELAY;
-    MS6 <= MS1 AND not_MS_2 after AND_GATE_DELAY;
-    MS7 <= MS1 AND MS(2)  after AND_GATE_DELAY;
-    MS8 <= MS2 AND not_MS_2 after AND_GATE_DELAY;
-    MS9 <= MS2 AND MS(2)  after AND_GATE_DELAY;
-    MS10 <= MS3 AND not_MS_2 after AND_GATE_DELAY;
-    MS11 <= MS3 AND MS(2) after AND_GATE_DELAY;
+    MS_0 <= not_MS_1  AND not_MS_0 after AND_GATE_DELAY;
+    MS_1 <= MS(1) AND not_MS_0 after AND_GATE_DELAY;
+    MS_2 <= not_MS_1 AND MS(0) after AND_GATE_DELAY;
+    MS_3 <= MS(1) AND MS(0) after AND_GATE_DELAY;
+    MS_4 <= MS_0 AND not_MS_2 after AND_GATE_DELAY;
+    MS_5 <= MS_0 AND MS(2) after AND_GATE_DELAY;
+    MS_6 <= MS_1 AND not_MS_2 after AND_GATE_DELAY;
+    MS_7 <= MS_1 AND MS(2)  after AND_GATE_DELAY;
+    MS_8 <= MS_2 AND not_MS_2 after AND_GATE_DELAY;
+    MS_9 <= MS_2 AND MS(2)  after AND_GATE_DELAY;
+    MS_10 <= MS_3 AND not_MS_2 after AND_GATE_DELAY;
+    MS_11 <= MS_3 AND MS(2) after AND_GATE_DELAY;
     
-    Zero_MS4 <= MS4 AND Zero after AND_GATE_DELAY;
-    ZFlag_MS5 <= MS5 AND Z_Flag after AND_GATE_DELAY;
-    VFlag_MS6 <= MS6 AND V_Flag after AND_GATE_DELAY;
-    One_MS7 <= MS7 AND One after AND_GATE_DELAY;
-    CFlag_MS8 <= MS8 AND C_Flag after AND_GATE_DELAY;
-    NFlag_MS9 <= MS9 AND N_Flag after AND_GATE_DELAY;
-    NotCFlag_MS10 <= MS10 AND C_Flag_Not after AND_GATE_DELAY;
-    NotZFLag_MS11 <= MS11 AND Z_Flag_Not after AND_GATE_DELAY;
+    Zero_MS_4 <= MS_4 AND Zero after AND_GATE_DELAY;
+    ZFlag_MS_5 <= MS_5 AND Z_Flag after AND_GATE_DELAY;
+    VFlag_MS_6 <= MS_6 AND V_Flag after AND_GATE_DELAY;
+    One_MS_7 <= MS_7 AND One after AND_GATE_DELAY;
+    CFlag_MS_8 <= MS_8 AND C_Flag after AND_GATE_DELAY;
+    NFlag_MS_9 <= MS_9 AND N_Flag after AND_GATE_DELAY;
+    NotCFlag_MS_10 <= MS_10 AND C_Flag_Not after AND_GATE_DELAY;
+    NotZFLag_MS_11 <= MS_11 AND Z_Flag_Not after AND_GATE_DELAY;
     
-    OR0 <= Zero_MS4 OR ZFlag_MS5 after OR_GATE_DELAY;
-    OR1 <= VFlag_MS6 OR One_MS7 after OR_GATE_DELAY;
-    OR2 <= CFlag_MS8 OR NFlag_MS9  after OR_GATE_DELAY;
-    OR3 <= NotCFlag_MS10 OR NotZFLag_MS11 after OR_GATE_DELAY;
-    OR4 <= OR0 OR OR1 after OR_GATE_DELAY;
-    OR5 <= OR2 OR OR3 after OR_GATE_DELAY;
-    OR6 <= OR4 OR OR5 after OR_GATE_DELAY;
+    OR_0 <= Zero_MS_4 OR ZFlag_MS_5 after OR_GATE_DELAY;
+    OR_1 <= VFlag_MS_6 OR One_MS_7 after OR_GATE_DELAY;
+    OR_2 <= CFlag_MS_8 OR NFlag_MS_9  after OR_GATE_DELAY;
+    OR_3 <= NotCFlag_MS_10 OR NotZFLag_MS_11 after OR_GATE_DELAY;
+    OR_4 <= OR_0 OR OR_1 after OR_GATE_DELAY;
+    OR_5 <= OR_2 OR OR_3 after OR_GATE_DELAY;
+    OR_6 <= OR_4 OR OR_5 after OR_GATE_DELAY;
     
-    CAR <= OR6;
+    CAR <= OR_6;
 
 end Behavioral;
