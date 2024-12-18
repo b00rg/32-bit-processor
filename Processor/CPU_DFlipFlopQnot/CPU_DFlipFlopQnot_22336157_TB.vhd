27a0cf9bb40c1f06bbdc839b325ce5e5dae24e14
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-12-18 13:52:28 +0000

    Update CPU_DFlipFlopQnot_22336157_TB.vhd

--- Processor/CPU_DFlipFlopQnot/CPU_DFlipFlopQnot_22336157_TB.vhd
+++ Processor/CPU_DFlipFlopQnot/CPU_DFlipFlopQnot_22336157_TB.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: Emma Burgess
 -- 
--- Create Date: 17.12.2024 16:55:20
+-- Create Date: 16.12.2024 15:35:16
 -- Design Name: 
--- Module Name: CPU_DFlipFlop_Qnot_22336157_TB - Simulation
+-- Module Name: CPU_DFlipFlop_Qnot_22336157 - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,72 +31,48 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity CPU_DFlipFlop_Qnot_22336157_TB is
-   -- Port ( );
-end CPU_DFlipFlop_Qnot_22336157_TB;
+entity CPU_DFlipFlop_Qnot_22336157 is
+    Port (
+        Clock, D, Reset : in STD_LOGIC;   -- single select bit
+        Q  : out STD_LOGIC  -- 17-bit output
+    );
+end CPU_DFlipFlop_Qnot_22336157;
 
-architecture Simulation of CPU_DFlipFlop_Qnot_22336157_TB is
-component CPU_DFlipFlop_Qnot_22336157 is
-    Port (Clock : in std_logic;
-          D : in std_logic;
-          Reset : in std_logic; 
-          Q : out std_logic;
-          Q_not : out std_logic  );
-end component;
+architecture Behavioral of CPU_DFlipFlop_Qnot_22336157 is
 
-signal CLK_TB, D_TB, Reset_TB, Q_TB, Q_Not_TB : std_logic := '0';
-   constant StudentID : STD_LOGIC_VECTOR (27 downto 0) := x"154D29D";
-constant PERIOD : time := 300 ns;
-
-begin
-
-uut : CPU_DFlipFlop_Qnot_22336157 Port Map
-            (   Clock => CLK_TB,
-                D => D_TB,
-                Reset => Reset_TB,
-                Q => Q_TB,
-                Q_not => Q_not_TB 
-            );
-
-CLK_TB <= NOT Clk_TB after PERIOD/2;
-stim_proc: process
-   begin	
+   constant AND_gate_delay : Time := 8ns;      -- least significant digit 6 = 5 + 1
+   constant NAND_gate_delay : Time := 6ns;     -- next more significant digit 3 = 2 + 1
+   constant OR_gate_delay : Time := 2ns;       -- next more significant digit 8 = 7 + 1
+   constant NOR_gate_delay : Time := 7ns;      -- next more significant digit 6 = 5 + 1
+   constant XOR_gate_delay : Time := 4ns;      -- next more significant digit 4 = 3 + 1
+   constant XNOR_gate_delay : Time := 4ns;     -- next more significant digit 4 = 3 + 1
+   constant NOT_gate_delay : Time := 3ns;      -- next more significant digit 7 = 6 + 1
    
-      Reset_TB <= '1'; 
-      wait for PERIOD;
-      
-      D_TB <= '1'; 
-      Reset_TB <= '0';
-      wait for PERIOD;
-      
-      D_TB <= '0'; 
-      wait for PERIOD/4;
-      
-      D_TB <= '1'; 
-      wait for PERIOD/4;
-      
-      D_TB <= '0';
-      wait for PERIOD/4;  
-      
-      D_TB <= '1'; 
-      wait for PERIOD/4;
-      
-      D_TB <= '0';
-      wait for PERIOD/4;
-      
-      D_TB <= '1'; 
-      wait for PERIOD/4;
-      
-      D_TB <= '0';
-      wait for PERIOD/4;
-      
-      D_TB <= '1'; 
-      wait for PERIOD/4;
-      
-      D_TB <= '0';
-      wait for PERIOD/4;
-      wait;
-
-    end process;
-
-end Simulation;
+   constant StudentID : STD_LOGIC_VECTOR (27 downto 0) := x"154D29D";
+   
+   signal Z, R0, R, Reset_not, L, Y0, Y, X0, K, S0, S, M, Q_t0, Q_not_t0, Q_t, Q_not_t, Q_not_t0_Negated : STD_LOGIC;
+   
+begin
+    Reset_not <= not Reset after NOT_gate_delay;
+    Z <= Clock and S after AND_gate_delay;  
+    
+    R0 <= Z and Y after AND_gate_delay;    
+    R <= not R0 after NOT_gate_delay;   
+    
+    L <= R and D after AND_gate_delay;     
+    Y0 <= L and Reset_not after AND_gate_delay; 
+    Y <= not Y0 after NOT_gate_delay; 
+    
+    X0 <= Y and S after AND_gate_delay;   
+    K <= X0 and Clock after AND_gate_delay;
+    S0 <= K and Reset_not after AND_gate_delay;
+    S <= not S0 after NOT_gate_delay;  
+    
+    Q_not_t0 <= M and Reset_not after AND_gate_delay; 
+    M <= Q_t and R after AND_gate_delay;   
+    Q_t0 <= S and Q_not_t0_Negated after AND_gate_delay; 
+    Q_not_t0_Negated <= not Q_not_t0 after NOT_gate_delay; 
+    Q_t <= not Q_t0 after NOT_gate_delay;
+    Q <= Q_t;                
+    
+end Behavioral;
